d66ff737e029625a00efbf1c500086b919c016e2
nelsonkimaiga <nelsonkimaiga@aol.com>
2019-01-24 16:56:31 +0300

    app boostrap

---
+++ src/test/java/com/example/userRestAPI/UserRestApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.userRestAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserRestApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
